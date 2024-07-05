fee436da93698a05834ce5054443cb748c208432
F. Pisot <fps1001@alu.ubu.es>
2024-07-05 14:07:37 +0200

    error 400 al conseguir los datos.

--- mockup_mobility/lib/main.dart
+++ mockup_mobility/lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Prueba Importación WMS a OSM',
       home: Scaffold(
-        body: const WMSMap(),
+        body: WMSMap(),
       ),
     );
   }
@@ -29,7 +29,7 @@ class WMSMap extends StatelessWidget {
         options: const MapOptions(
           initialCenter:
               LatLng(40.95821, -5.67413), // Parque Elio Antonio Nebrija
-          initialZoom: 18.0,
+          initialZoom: 17.0,
         ),
         children: [
           TileLayer(
@@ -38,15 +38,15 @@ class WMSMap extends StatelessWidget {
           TileLayer(
             wmsOptions: WMSTileLayerOptions(
               baseUrl:
-                  'https://ide.aytosalamanca.es/geoserver/ide_salamanca_life/wms?',
-              layers: const ['ide_salamanca_life:trazado_BIC_via_de_la_plata'],
+                  'https://ide.aytosalamanca.es/geoserver/ide_salamanca_life/wms',
+              layers: const ['zonaslife_rdp_v4_sep21_25830'],
               format: 'image/png',
-              transparent: false,
+              transparent: true,
               //version: '1.3.0',
               otherParameters: const {
                 'service': 'WMS',
                 'request': 'GetMap',
-                'styles': '',
+                'styles': 'ide_salamanca_life:zonaslife',
                 'crs': 'EPSG:25830', // Replace with the correct CRS if needed
                 //'crs': 'EPSG:3857',
               },
