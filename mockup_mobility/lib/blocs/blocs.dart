b5f07dc6f720633142f45693a0b39946f8139f98
F. Pisot <fps1001@alu.ubu.es>
2024-07-08 13:56:21 +0200

    typos e importaciones corregidas.

--- mockup_mobility/lib/blocs/blocs.dart
+++ mockup_mobility/lib/blocs/blocs.dart
@@ -1,3 +1,5 @@
 //Archivo barril de blocs
 
-export 'wms/wms_bloc.dart';
+export './wms/wms_bloc.dart';
+export './wms/wms_event.dart';
+export './wms/wms_state.dart';
--- mockup_mobility/lib/screens/wms_map.dart
+++ mockup_mobility/lib/screens/wms_map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import '../blocs/blocs.dart';
